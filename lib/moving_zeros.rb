376c243483bff654754373a2c1a9a8755eebb023
Will Howes <williameldenhowes@gmail.com>
2019-09-29 09:30:01 +0100

    Handles array with one zero and other two other characters

--- lib/moving_zeros.rb
+++ lib/moving_zeros.rb
@@ -1,4 +1,4 @@
 def moving_zeros(array_to_handle)
   return array_to_handle if array_to_handle.length == 1
-  array_to_handle.reverse
+  array_to_handle.sort.reverse
 end
--- spec/moving_zeros_spec.rb
+++ spec/moving_zeros_spec.rb
@@ -7,4 +7,8 @@ describe "#moving_zeros" do
   it 'returns [1,0] when given an input of [0,1]' do
     expect(moving_zeros([0,1])).to eq([1,0])
   end
+
+  it 'returns [1,1,0] when given an input of [1,0,1]' do
+    expect(moving_zeros([1,0,1])).to eq([1,1,0])
+  end
 end
